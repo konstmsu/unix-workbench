ca495542dc3fdf4b64002bec84a725e7a227d127
Konstantin Spirin <konstmsu@gmail.com>
2018-10-10 00:24:11 +0800

    Just added Fibonacci

---
+++ fibonacci.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+fib_cache=(0 1)
+
+fib ()
+{
+    local n=$1
+    while [ ${#fib_cache[@]} -lt $n ]; do
+        fib_cache+=($(bc <<< "${fib_cache[-1]} + ${fib_cache[-2]}"))
+        # >&2 echo "Updated: ${fib_cache[@]} for n=$n"
+    done
+    echo ${fib_cache[@]:0:$n}
+}
+
+fib_tests ()
+{
+    output=fibonacci_actual.out
+    > $output
+    test()
+    {
+        local n=$1
+        echo "$n -> $(fib $n)" >>$output
+    }
+    test 1
+    test 2
+    test 3
+    test 4
+    test 5
+    test 6
+    test 7
+    test 8
+    test 9
+    test 10
+    test 11
+    test 12
+    test 13
+    test 99
+    test 201
+    
+    diff -y fibonacci_expected.out $output
+}
---
+++ math.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+expr 5 + 2
+expr 5 - 2
+expr 5 \* 2
+expr 5 / 2
